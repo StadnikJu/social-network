19199bfc68ae07c622404b6c5df63babca81c312
StadnikJu <stadnikjulia14@icloud.com>
2026-07-17 16:49:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Profile } from "./components/Profile";
+import { Header } from "./components/Header";
+import { NavBar } from "./components/NavBar";
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <NavBar />
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/017/764/201/small/bird-freedom-fly-animal-line-art-linear-simple-abstract-minimalist-circle-border-logo-design-vector.jpg" />
+    </header>
+  );
+};
